34361053739fd6bc386b1ce54a5d7abfdc106170
Dmitry Yacenko <dmitryyacenko@gmail.com>
2019-04-19 09:42:50 +0300

    Update UIKit version && remove examples

--- README.md
+++ README.md
@@ -3,13 +3,13 @@ Meteor UIkit
 > [UIKit](http://www.getuikit.com) 3.x packaged for Meteor forked from [meteor-uikit](https://github.com/alexandesigner/meteor-uikit)
 
 ## To install
-``meteor add alexandesigner:meteor-uikit``
+``meteor add unclefeudor:meteor-uikit``
 
 ## Bug report, suggestions and improvements
-[Issues](https://github.com/alexandesigner/meteor-uikit/issues)
+[Issues](https://github.com/dmitryyacenko/meteor-uikit/issues)
 
 ## License
 meteor-uikit is available under MIT-License and can therefore be used in any project free of charge.
 
 ## Contributors
-- Allan Alexandre (alexandesigner@gmail.com)
+- Uncle Feudor (https://t.me/unclefeudor)
--- package.js
+++ package.js
@@ -1,8 +1,8 @@
 Package.describe({
-  name: 'alexandesigner:meteor-uikit',
+  name: 'unclefeudor:meteor-uikit',
   version: '1.0.2',
   summary: 'A lightweight and modular front-end framework for developing fast and powerful web interfaces',
-  git: 'https://github.com/alexandesigner/meteor-uikit',
+  git: 'https://github.com/dmitryyacenko/meteor-uikit',
   documentation: 'README.md'
 });
 
@@ -14,12 +14,12 @@ Package.onUse(function (api, where) {
   const assetPath = path.join('src');
 
   //js
-  api.addFiles(path.join(assetPath, 'js', 'uikit.js'), 'client');
+  api.addFiles(path.join(assetPath, 'js', 'uikit.min.js'), 'client');
 
   //css
-  api.addFiles(path.join(assetPath, 'css', 'uikit.css'), 'client');
+  api.addFiles(path.join(assetPath, 'css', 'uikit.min.css'), 'client');
 
   //icons
-  api.addAssets(path.join(assetPath, 'images', 'icons.svg'), 'client');
+  api.addAssets(path.join(assetPath, 'js', 'uikit-icons.min.js'), 'client');
 
 });
